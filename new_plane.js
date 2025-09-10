d0e791267c883f083ef0b83171fd780bcca99453
Harsh <saizalkhandelwal>
2025-09-10 15:05:05 +0530

    chnaed

--- new_plane.js
+++ new_plane.js
@@ -12,5 +12,4 @@
 //omg. i love an =d i want ot kamjnwjewj
 
 //this is not in my idea but i can sy that you are my nigaa
-// i don.y have any gf to celebrate 
-
+// i don.y have any gf to celebrate
